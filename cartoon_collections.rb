cfc71e23111c18493b2ada002a7cc78327c81d94
redebron20 <redebron20@gmail.com>
2020-10-29 20:58:51 -0400

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,6 +21,6 @@ def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |cheese|
-    cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
+    cheese_types.include?(cheese)
   end
 end
